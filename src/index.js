9ed226a78a61f4841e9ca2308e61e5c3882b9bc7
kghugo <howtostudycantonese@gmail.com>
2020-07-23 19:12:17 +0800

    :wrench: Add config for CJS

---
+++ src/index.js
@@ -0,0 +1,57 @@
+import {
+  getSavedData
+} from './utilities/helper.js'
+
+import {
+  render,
+  html,
+  svg
+} from 'lighterhtml'
+
+class Code {
+  constructor (customOptions) {
+    const defaultOptions = {
+      i18n: {
+        locale: 'en-US',
+        rtl: 'ltr',
+        translations: {
+
+        }
+      },
+      controllers: [
+
+      ]
+    }
+
+    this.options = {
+      ...defaultOptions,
+      ...customOptions
+    }
+  }
+
+  // Data attribute is a must
+  render (i18n, savedData) {
+    return html.node`<div class="freedom-editor-blocks ${this.constructor.name}-block" data-block-type="${this.constructor.name}">
+    <pre>
+    <code contenteditable>${getSavedData(savedData)}</code>
+    </pre>
+    </div>`
+  }
+
+  save (block) {
+    const codeField = block.querySelector('code')
+    if (codeField.textContent === '') {
+      return
+    }
+    return {
+      type: this.constructor.name,
+      data: {
+        code: codeField.textContent
+      }
+    }
+  }
+}
+
+export {
+  Code
+}
